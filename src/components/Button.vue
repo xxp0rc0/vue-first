7edf53aa1501a5f1acead6f30c1a71f5e34961f1
hoag.a <hoanganh1869@gmail.com>
2022-10-13 00:05:38 +0700

    2nd commit

---
+++ src/components/Button.vue
@@ -0,0 +1,21 @@
+<template>
+    <button @click="onClick()" :style="{backgroundColor: color}" class="btn">{{text}}</button>
+</template>
+<script>
+export default {
+    name: "Button",
+    props: {
+        text: String,
+        color: String,
+    },
+    methods: {
+        onClick() {
+           this.$emit('btn-click')
+           console.log('click')
+        }
+    }
+}
+</script>
+<style scoped>
+
+</style>
